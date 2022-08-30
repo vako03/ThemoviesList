c7e8d023afd6381042289e30399fdd5ea04a479c
vako03 <vako.mekhashishvili@gmail.com>
2022-08-30 21:12:49 +0400

    hide progress bar

--- app/src/main/java/com/benten/themovieslist/MovieListFragment.kt
+++ app/src/main/java/com/benten/themovieslist/MovieListFragment.kt
@@ -4,8 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import com.benten.themovieslist.apis.RetrofitBuilder
 import com.benten.themovieslist.databinding.LayoutMovieListFragmentBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MovieListFragment : Fragment() {
     private var _binding: LayoutMovieListFragmentBinding? = null
@@ -24,11 +31,20 @@ class MovieListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        CoroutineScope(IO).launch {
+            val popularsResponse = RetrofitBuilder.moviesApi.getPopularMovies(APY_KEY)
+            withContext(Main){
+                binding.progressBar.isVisible = false
+            }
 
+        }
     }
 
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
     }
+    companion object {
+        const val APY_KEY=("843c612d1207fdec63f0e6a5fd426d68")
+        }
 }
